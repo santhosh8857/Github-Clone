1b8db5edd0e18b1b34bb7f584471cf56d5116a67
Santhosh <82262076+santhosh8857@users.noreply.github.com>
2021-07-11 01:10:35 +0530

    Updated file with search repo and files

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 document.querySelector('#btn').addEventListener('click', getData);
 
-let reposWrapper = document.querySelector('#repos')
-let reposList = document.querySelector('#reposList');
+let reposWrapper = document.querySelector('#repos')//selection
+let choose = document.querySelector('#choose');
 
+let mainFileWrapper = document.querySelector('#main-file-wrapper'); //container for file
+let fileWrapper = document.querySelector('#file-wrapper'); //ul
 
 function getData(e) {
+
+   reposWrapper.innerHTML = '';
+   choose.innerHTML = '';
+   
    let userName = document.querySelector('#search').value;
 
    let urlRepos = `https://api.github.com/users/${userName}/repos`;
@@ -12,6 +18,7 @@ function getData(e) {
    fetch(urlRepos)
       .then((resp) => resp.json())
       .then((repos) => {
+         if(repos.message === "Not Found") throw alert('Please Enter a valid UserName');
          console.log(repos);
          getRepos(repos,userName);
       })
@@ -24,17 +31,54 @@ function getRepos(data, user) {
 
    let heading = document.createElement('h3');
    heading.innerText = `"${user}" has ${data.length} repositories`;
-   heading.classList.add('ml-2')
+   heading.classList.add('ml-2', 'text-center', 'text-light','mb-4');
 
-   data.forEach(function(repos){
+   let selection = document.createElement('select');
+   selection.classList.add('custom-select');
+   selection.innerHTML = '<option selected> Kindly Choose desired Repository to view the files...</option>'
+
+    data.forEach(function(repos){
+      let options = document.createElement('option');
+      options.setAttribute("value", repos.name);
+      options.innerText = repos.name;
+      
+      selection.appendChild(options);
+   });
+ 
+   choose.appendChild(selection);
+   reposWrapper.append(heading, choose);
+
+   selection.addEventListener('change', (e) => {
+      mainFileWrapper.innerHTML='';
+      fileWrapper.innerHTML ='';
+      let repoName = e.target.value;
+      let contentUrl = `https://api.github.com/repos/${user}/${repoName}/contents/`;
+      
+      fetch(contentUrl)
+         .then(resp => resp.json())
+         .then(content => getFiles(content, repoName))
+         .catch(err => console.log(err));
+   });
+
+}
+
+function getFiles(fileList, repository) {
+   console.log(fileList);
+
+   let heading = document.createElement('h4');
+   heading.innerText = `"${repository}" repository contains ${fileList.length} files`;
+   heading.classList.add('text-center', 'text-light','mb-4');
+
+   fileList.forEach(function(file){
       let list = document.createElement('li');
-      list.classList.add('list-group-item');
-      list.innerText = repos.name;
+      list.classList.add('list-group-item', 'text-grey','list-width','list-group-item-light');
+      list.innerText = file.name;
 
-      reposList.appendChild(list);
+      fileWrapper.appendChild(list);
    })
 
-   reposWrapper.append(heading,reposList);
+   mainFileWrapper.appendChild(heading);
+   mainFileWrapper.appendChild(fileWrapper);
 
 }
 
@@ -53,5 +97,11 @@ function getRepos(data, user) {
 //       document.querySelector('tbody').appendChild(row);
 // }
 
+  // data.forEach(function(repos){
+   //    let list = document.createElement('li');
+   //    list.classList.add('list-group-item', 'text-grey');
+   //    list.innerText = repos.name;
 
+   //    reposList.appendChild(list);
+   // })
 
